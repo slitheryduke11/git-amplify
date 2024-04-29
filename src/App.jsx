35395eb8c06f4c3ee564f6a20bcf4a3b17fb6837
Hedguhar Domínguez <hedguhar2511@gmail.com>
2024-04-29 11:47:36 -0600

    Added beautiful messages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,33 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(1)
+  const beautifulWords = [
+    'Te amo en este y en todos los universos',
+    'Eres mi niña hermosa',
+    'Amo pasar tiempo a tu lado',
+    'Eres mi mejor amiga',
+    'Eres mi confidente',
+    'Eres mi compañera de vida',
+    'Me gustan tus meloncitos',
+    'Amo verte sonreir',
+    'Cada que veas esto recuerda que te amo',
+    'Me encantas',
+    'Eres la niña de mis ojos',
+    'Gracias por retarme a besarte',
+    'Quédate conmigo para siempre',
+    'Haces mi vida más divertida',
+    'Amo ir al cine contigo',
+    'Eres la única con quien quiero ver videos de Dross',
+    'Amo ver tus ojitos brillar',
+    'Haces que mi pecho se sienta feliz',
+    'Siempre seré tu compañero de aventuras',
+    'Soy solo tuyo',
+    'Sé siempre mía'
+  ];
+  const randomWord = beautifulWords[Math.floor(Math.random() * beautifulWords.length)];
+
+  const [word, setWord] = useState(randomWord);
+
 
   return (
     <>
@@ -19,7 +46,7 @@ function App() {
         </button>
       </div>
       <p className="read-the-docs">
-        En este y en todos los universos.
+        {word}
       </p>
     </>
   )
